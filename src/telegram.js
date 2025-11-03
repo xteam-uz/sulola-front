9c0ef7e9d8b060ad8f194497b95e384406406f3e
mirrrjr <mirrrrrr@yahoo.com>
2025-11-03 13:08:36 +0500

    edited

---
+++ src/telegram.js
@@ -0,0 +1,22 @@
+import WebApp from '@twa-dev/sdk';
+
+export const initTelegramApp = () => {
+    WebApp.enableClosingConfirmation();
+    WebApp.setHeaderColor('#2481cc');
+    WebApp.setBackgroundColor('#ffffff');
+
+    // Ready event
+    WebApp.ready();
+};
+
+export const getUserData = () => {
+    return WebApp.initDataUnsafe?.user || null;
+};
+
+export const sendDataToBot = (data) => {
+    WebApp.sendData(JSON.stringify(data));
+};
+
+export const closeApp = () => {
+    WebApp.close();
+};
