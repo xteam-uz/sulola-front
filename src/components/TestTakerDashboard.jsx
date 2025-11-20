0940a6f6a671c13e1d8c33e1f7778b5e77b3bc72
mirrrjr <mirrrrrr@yahoo.com>
2025-11-20 12:37:34 +0500

    add Lottie tg gifs and some changes

--- src/components/TestTakerDashboard.jsx
+++ src/components/TestTakerDashboard.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { ChevronRight, Clock, FileText } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import { useStateContext } from "../contexts/ContextProvider";
