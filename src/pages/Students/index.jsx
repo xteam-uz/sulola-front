e1c4f40db6c1d2b3c289eced3ea0675d143a683e
mirrrjr <mirrrrrr@yahoo.com>
2025-12-02 11:54:02 +0500

    remove user plus button

--- src/pages/Students/index.jsx
+++ src/pages/Students/index.jsx
@@ -66,9 +66,9 @@ export const Students = () => {
             </div>
 
             {/* Add Button */}
-            <button className="fixed bottom-20 right-5 bg-blue-500 hover:bg-blue-600 text-white p-4 rounded-full shadow-lg transition-all">
+            {/* <button className="fixed bottom-20 right-5 bg-blue-500 hover:bg-blue-600 text-white p-4 rounded-full shadow-lg transition-all">
                 <Plus size={22} />
-            </button>
+            </button> */}
 
             {/* Bottom Navigation */}
             <FooterNavbar />
